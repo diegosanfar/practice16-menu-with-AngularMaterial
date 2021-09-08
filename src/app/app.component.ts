f92dba57bece00dafab27b03e6b604a86a0e78e4
diegosanfar <diegosantosfaria@hotmail.com>
2021-09-08 18:28:44 -0300

    Ajuste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+const urlBase = 'http://localhost:3000';
+
+interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'pratica-aula16';
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  produtos: Produto[] = [];
+  form = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    preco: new FormControl(0, [Validators.required, Validators.min(1)]),
+  });
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient.get<Produto[]>(`${urlBase}/produtos`).subscribe((dados) => {
+      this.produtos = dados;
+    });
+  }
+
+  adicionarProduto() {
+    const produto = this.form.value;
+    this.httpClient
+      .post<Produto>(`${urlBase}/produtos`, produto)
+      .subscribe((novoProduto) => {
+        this.produtos = [...this.produtos, novoProduto];
+      });
+  }
+
+  excluir(produto: Produto) {
+    this.httpClient
+      .delete(`${urlBase}/produtos/${produto.id}`)
+      .subscribe(() => {
+        const index = this.produtos.indexOf(produto);
+        this.produtos = [
+          ...this.produtos.slice(0, index),
+          ...this.produtos.slice(index + 1),
+        ];
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import localePt from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+registerLocaleData(localePt);
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTable, MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
